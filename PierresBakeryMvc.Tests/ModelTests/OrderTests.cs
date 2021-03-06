66db9670214828cf4450f2d3251b9d2b8b84e243
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-03-05 16:26:42 -0800

    Pass test to get vendorName

--- PierresBakeryMvc.Tests/ModelTests/OrderTests.cs
+++ PierresBakeryMvc.Tests/ModelTests/OrderTests.cs
@@ -12,8 +12,20 @@ namespace PierresBakeryMvc.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order("test");
+      Order newOrder = new Order("Test Vendor");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+
+    [TestMethod]
+    public void GetVendorName_ReturnsVendorName_String()
+    {
+      string vendorName = "Test Vendor";
+
+      Order newOrder = new Order(vendorName);
+      string result = newOrder.VendorName;
+
+      Assert.AreEqual(vendorName, result);
+    }
+
   }
 }
--- PierresBakeryMvc/Models/Order.cs
+++ PierresBakeryMvc/Models/Order.cs
@@ -1,14 +1,23 @@
-// using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 
 namespace PierresBakeryMvc.Models
 {
   public class Order
   {
     public string VendorName { get; set; }
+    // public string Description { get; set; }
+    // // public DateTime Date { get; }
+    // public int OrderPrice { get; set; }
+    // public int Id { get; }
+    // private static List<Order> _instances = new List<Order> { };
+
 
     public Order(string vendorName)
     {
       VendorName = vendorName;
+      // Description = description;
+      // OrderPrice = orderPrice;
     }
 
   }
